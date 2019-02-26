d7339a64c1e933caf66e4c13f6341da8f3f955c2
Rolljee <n.riquelmebareiro@gmail.com>
2019-02-26 22:04:43 +0100

    Updates

---
+++ precache-manifest.9eb66dfbc9c098032ea0866fd346c35a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f43017bfeb32ca2e63e",
+    "url": "/ogame-ui/static/css/main.6795c4d2.chunk.css"
+  },
+  {
+    "revision": "2f43017bfeb32ca2e63e",
+    "url": "/ogame-ui/static/js/main.2f43017b.chunk.js"
+  },
+  {
+    "revision": "6b826bdf38480c462032",
+    "url": "/ogame-ui/static/js/1.6b826bdf.chunk.js"
+  },
+  {
+    "revision": "65d692c3f1a69016decc",
+    "url": "/ogame-ui/static/js/runtime~main.65d692c3.js"
+  },
+  {
+    "revision": "abb8b2b8632cf89c685200f9bc2b4a33",
+    "url": "/ogame-ui/index.html"
+  }
+];
