2c80ffa4dae836139e0866ff2bbf6955c2f52982
Rolljee <n.riquelmebareiro@gmail.com>
2019-07-23 16:41:39 +0200

    Updates

---
+++ precache-manifest.05582cad0c0c940ea4f2d824b7fd0d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e79583a83e6142cd8188a0a6f502fb0",
+    "url": "/ogame-ui/index.html"
+  },
+  {
+    "revision": "1f941116f6146efc11c2",
+    "url": "/ogame-ui/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "db4e7065755b1f7ebc29",
+    "url": "/ogame-ui/static/js/2.ff6ae4cd.chunk.js"
+  },
+  {
+    "revision": "1f941116f6146efc11c2",
+    "url": "/ogame-ui/static/js/main.38f2ace0.chunk.js"
+  },
+  {
+    "revision": "9d4f8a0700861d55aba0",
+    "url": "/ogame-ui/static/js/runtime~main.fc768b41.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ogame-ui/precache-manifest.46578a7730347ae96c1dbe52230c6014.js"
+  "/ogame-ui/precache-manifest.05582cad0c0c940ea4f2d824b7fd0d2d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ogame-ui/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ogame-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
