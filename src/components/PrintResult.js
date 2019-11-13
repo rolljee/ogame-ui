e75dd5d0d73ec89febcf2cefa2dd83f10567df40
rolljee <n.riquelmebareiro@gmail.com>
2019-11-13 12:05:55 +0100

    UI improvements

--- src/components/PrintResult.js
+++ src/components/PrintResult.js
@@ -7,18 +7,16 @@ class PrintResult extends React.Component {
   render() {
     const { metal, crystal, deut, selected } = this.props;
     let text = "";
+    const deutText = deutText;
+    const metalText = metalText;
+    const crystalText = crystalText;
+
     if (selected === "deut") {
-      text = `Selling ${this.prettify(deut)} deuterium for ${this.prettify(
-        metal
-      )} metal and ${this.prettify(crystal)} crystal`;
+      text = `${deutText} deut \n ${metalText} metal \n ${crystalText} crystal`;
     } else if (selected === "crystal") {
-      text = `Selling ${this.prettify(crystal)} crystal for ${this.prettify(
-        metal
-      )} metal and ${this.prettify(deut)} deuterium`;
+      text = `${crystalText} crystal \n ${metalText} metal \n ${deutText} deut`;
     } else if (selected === "metal") {
-      text = `Selling ${this.prettify(metal)} metal for ${this.prettify(
-        deut
-      )} deuterium and ${this.prettify(crystal)} crystal`;
+      text = `${metalText} metal \n ${deutText} deut \n ${crystalText} crystal`;
     } else {
       text = "Nothing to sell yet";
     }
