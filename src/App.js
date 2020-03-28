79c936c9f70d50227c7430e6af52cdb7634167fe
Rolljee <n.riquelmebareiro@gmail.com>
2020-03-28 13:22:57 +0100

    should fix route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -11,61 +11,41 @@ import Home from './Home';
 import Trader from './Trader/Trader';
 import Players from './Players/Players';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
+function App() {
+	const [background, setBackground] = useState(BACKGROUND.dark);
 
-		this.state = {
-			background: BACKGROUND.dark,
-		};
-
-		this.setBackground = this.setBackground.bind(this);
-	}
-
-	setBackground() {
-		this.setState({
-			background:
-				this.state.background === BACKGROUND.light
-					? BACKGROUND.dark
-					: BACKGROUND.light,
-		});
-	}
-
-	render() {
-		const { background } = this.state;
-		return (
-			<Layout background={background} setBackground={this.setBackground}>
-				<Router>
-					<Switch>
-						<Route path="trades">
-							<Trader />
-						</Route>
-						<Route path="players">
-							<Players />
-						</Route>
-						<Route path="market">
-							{/* <Home /> */}
-						</Route>
-						<Route path="mining">
-							{/* <Home /> */}
-						</Route>
-						<Route path="universes">
-							{/* <Home /> */}
-						</Route>
-						<Route path="score">
-							{/* <Home /> */}
-						</Route>
-						<Route path="alliances">
-							{/* <Home /> */}
-						</Route>
-						<Router path="/">
-							<Home />
-						</Router>
-					</Switch>
-				</Router>
-			</Layout >
-		);
-	}
+	return (
+		<Layout background={background} setBackground={() => setBackground(background === BACKGROUND.light ? BACKGROUND.dark : BACKGROUND.light)}>
+			<Router basename="ogame-ui">
+				<Switch>
+					<Route path="/trades">
+						<Trader />
+					</Route>
+					<Route path="/players">
+						<Players />
+					</Route>
+					<Route path="/market">
+						{/* <Home /> */}
+					</Route>
+					<Route path="/mining">
+						{/* <Home /> */}
+					</Route>
+					<Route path="/universes">
+						{/* <Home /> */}
+					</Route>
+					<Route path="/score">
+						{/* <Home /> */}
+					</Route>
+					<Route path="/alliances">
+						{/* <Home /> */}
+					</Route>
+					<Router path="/">
+						<Home />
+					</Router>
+				</Switch>
+			</Router>
+		</Layout >
+	)
 }
 
 export default App;
