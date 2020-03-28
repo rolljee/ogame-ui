897fc29e7d34c849520d6eed065b33da6b8b6ca1
Rolljee <n.riquelmebareiro@gmail.com>
2020-03-28 17:32:41 +0100

    UI improments on playerData

--- src/App.js
+++ src/App.js
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from "react-router-dom";
-
-import { BACKGROUND } from './components/constants';
-import Layout from './components/Layout';
-import Home from './Home';
-import Trader from './Trader/Trader';
-import Players from './Players/Players';
-
-class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			background: BACKGROUND.dark,
-		};
-
-		this.setBackground = this.setBackground.bind(this);
-	}
-
-	setBackground() {
-		this.setState({
-			background:
-				this.state.background === BACKGROUND.light
-					? BACKGROUND.dark
-					: BACKGROUND.light,
-		});
-	}
-
-	render() {
-		const { background } = this.state;
-		return (
-			<Layout background={background} setBackground={this.setBackground}>
-				<Router>
-					<Switch>
-						<Route path="/trades">
-							<Trader />
-						</Route>
-						<Route path="/players">
-							<Players />
-						</Route>
-						<Route path="/market">
-							{/* <Home /> */}
-						</Route>
-						<Route path="/mining">
-							{/* <Home /> */}
-						</Route>
-						<Route path="/universes">
-							{/* <Home /> */}
-						</Route>
-						<Route path="/score">
-							{/* <Home /> */}
-						</Route>
-						<Route path="/alliances">
-							{/* <Home /> */}
-						</Route>
-						<Router path="/">
-							<Home />
-						</Router>
-					</Switch>
-				</Router>
-			</Layout >
-		);
-	}
-=======
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -138,7 +68,6 @@ function App() {
 			</Router>
 		</>
 	);
->>>>>>> 71da4414635601f8a9a400209d506854b5d3e252
 }
 
 export default App;
